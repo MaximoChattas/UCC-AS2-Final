fccf336683f2381e8f987f0e6abf7ae91e6496f7
MaximoChattas <maxichattas@gmail.com>
2023-10-06 16:16:15 -0300

    Required fields in User DTO

--- User-Reservation-api/Backend/dto/user.go
+++ User-Reservation-api/Backend/dto/user.go
@@ -2,10 +2,10 @@ package dto
 
 type UserDto struct {
 	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	LastName string `json:"last_name"`
-	Dni      string `json:"dni"`
-	Email    string `json:"email"`
+	Name     string `json:"name" validate:"required"`
+	LastName string `json:"last_name" validate:"required"`
+	Dni      string `json:"dni" validate:"required"`
+	Email    string `json:"email" validate:"required"`
 	Password string `json:"password,omitempty"`
 	Role     string `json:"role"`
 }
