42f1e96f2cf0183441f77a931ba4321434d33421
MaximoChattas <maxichattas@gmail.com>
2023-10-06 13:28:08 -0300

    Router and mappings created

---
+++ User-Reservation-api/Backend/app/url_mappings.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"User-Reservation/controller"
+	log "github.com/sirupsen/logrus"
+)
+
+func mapUrls() {
+
+	// Add all methods and its mappings
+	router.POST("/user", controller.InsertUser)
+	router.GET("/user/:id", controller.GetUserById)
+	router.GET("/user", controller.GetUsers)
+	router.POST("/login", controller.UserLogin)
+
+	log.Info("Finishing mappings configurations")
+}
