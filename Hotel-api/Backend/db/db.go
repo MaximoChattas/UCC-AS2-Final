7d69f30df1f5b850fc11f581965fd2176fca6e6a
MaximoChattas <maxichattas@gmail.com>
2023-10-09 12:11:36 -0300

    MongoDB connection file created

---
+++ Hotel-api/Backend/db/db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoDb *mongo.Database
+var client *mongo.Client
+
+func Disconect_db() {
+
+	client.Disconnect(context.TODO())
+}
+
+func Init_db() {
+
+	clientOpts := options.Client().ApplyURI("mongodb://root:pass@localhost:27017/?authSource=admin&authMechanism=SCRAM-SHA-256")
+	cli, err := mongo.Connect(context.TODO(), clientOpts)
+	client = cli
+
+	if err != nil {
+		log.Info("Connection Failed to Open")
+		log.Fatal(err)
+	} else {
+		log.Info("Connection Established")
+	}
+
+	dbNames, err := client.ListDatabaseNames(context.TODO(), bson.M{})
+	if err != nil {
+		log.Info("Failed to get databases available")
+		log.Fatal(err)
+	}
+
+	MongoDb = client.Database("test")
+
+	fmt.Println("Available datatabases:")
+	fmt.Println(dbNames)
+
+}
