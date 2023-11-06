d31742f959f75264b34c0ad67c8917aa587fd933
YagoGandara <yago@gandara.com.ar>
2023-11-06 00:19:02 -0300

    corregir import solr.go

--- Search-api/solr/solr.go
+++ Search-api/solr/solr.go
@@ -1,8 +1,12 @@
 package solr
 
 import (
-	"github.com/rtt/Go-Solr"
+	"Search/dto"
+	"Search/service"
+	"encoding/json"
 	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
 )
 
 var SolrClient *solr.Connection
@@ -18,4 +22,44 @@ func InitSolr() {
 	} else {
 		log.Info("Connected to Solr successfully")
 	}
+
+	insertData()
+}
+
+func insertData() {
+
+	resp, err := http.Get("http://hotel:8080/hotel")
+
+	if err != nil {
+		log.Error("Error in HTTP request: ", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Error("Error reading response: ", err)
+		return
+	}
+
+	var hotelsDto dto.HotelsDto
+
+	err = json.Unmarshal(body, &hotelsDto)
+
+	if err != nil {
+		log.Error("Error parsing JSON: ", err)
+		return
+	}
+
+	for _, hotel := range hotelsDto {
+
+		err = service.HotelService.InsertUpdateHotel(hotel)
+	}
+
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
 }
