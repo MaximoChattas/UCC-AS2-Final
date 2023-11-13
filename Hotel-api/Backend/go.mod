c1e567628614a67471acf376569c87f3c2a7bb62
MaximoChattas <maxichattas@gmail.com>
2023-11-13 16:21:13 -0300

    Hotel service test implemented in Hotel API

--- Hotel-api/Backend/go.mod
+++ Hotel-api/Backend/go.mod
@@ -3,6 +3,7 @@ module Hotel
 go 1.20
 
 require (
+	github.com/NeowayLabs/wabbit v0.0.0-20210927194032-73ad61d1620e
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/karlseguin/ccache/v3 v3.0.5
@@ -23,6 +24,8 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -31,6 +34,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
@@ -43,7 +47,7 @@ require (
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ Hotel-api/Backend/service/hotel_test.go
@@ -0,0 +1,256 @@
+package service
+
+import (
+	"Hotel/client"
+	"Hotel/dto"
+	"Hotel/model"
+	"Hotel/queue"
+	"errors"
+	"github.com/NeowayLabs/wabbit"
+	"github.com/NeowayLabs/wabbit/amqptest"
+	"github.com/NeowayLabs/wabbit/amqptest/server"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"testing"
+)
+
+type testClient struct{}
+type testQueue struct{}
+
+var channel wabbit.Channel
+var mockQueue wabbit.Queue
+
+func init() {
+	client.HotelClient = testClient{}
+	queue.QueueProducer = testQueue{}
+	queue.QueueProducer.InitQueue()
+}
+
+func (t testClient) InsertHotel(hotel model.Hotel) model.Hotel {
+
+	if hotel.Name != "" {
+		objId, _ := primitive.ObjectIDFromHex("654cf68d807298d99186019f")
+		hotel.Id = objId
+	}
+
+	return hotel
+}
+
+func (t testClient) GetHotelById(id string) model.Hotel {
+	if id == "000000000000000000000000" {
+		return model.Hotel{}
+	}
+
+	objId, _ := primitive.ObjectIDFromHex(id)
+
+	return model.Hotel{
+		Id: objId,
+	}
+}
+
+func (t testClient) GetHotels() model.Hotels {
+	return model.Hotels{}
+}
+
+func (t testClient) DeleteHotelById(id string) error {
+
+	if id == "000000000000000000000000" {
+		return errors.New("hotel not found")
+	}
+
+	return nil
+}
+
+func (t testClient) UpdateHotelById(hotel model.Hotel) model.Hotel {
+
+	if hotel.Id.Hex() == "000000000000000000000000" {
+		return model.Hotel{}
+	}
+
+	return hotel
+}
+
+func (t testQueue) InitQueue() {
+
+	fakeServer := server.NewServer("amqp://localhost:5672/%2f")
+	err := fakeServer.Start()
+
+	if err != nil {
+		log.Info("Failed to start server")
+		log.Fatal(err)
+	}
+
+	connection, err := amqptest.Dial("amqp://localhost:5672/%2f")
+
+	if err != nil {
+		log.Info("Failed to connect to RabbitMQ")
+		log.Fatal(err)
+	} else {
+		log.Info("RabbitMQ connection established")
+	}
+
+	channel, err = connection.Channel()
+
+	if err != nil {
+		log.Info("Failed to open channel")
+		log.Fatal(err)
+	}
+
+	defer channel.Close()
+
+	mockQueue, err = channel.QueueDeclare("hotel", wabbit.Option{
+		"durable":    true,
+		"autoDelete": false,
+		"exclusive":  false,
+		"noWait":     false,
+	})
+
+	if err != nil {
+		log.Info("Failed to declare a queue")
+		log.Fatal(err)
+	} else {
+		log.Info("Queue declared")
+	}
+}
+
+func (t testQueue) Publish(body []byte) error {
+
+	err := channel.Publish(
+		"",
+		mockQueue.Name(),
+		body,
+		wabbit.Option{"contentType": "application/json"})
+
+	if err != nil {
+		log.Debug("Error while publishing message", err)
+		return err
+	}
+
+	return nil
+}
+
+func TestInsertHotel_Error(t *testing.T) {
+
+	a := assert.New(t)
+	var hotelDto dto.HotelDto
+
+	_, err := HotelService.InsertHotel(hotelDto)
+
+	expectedResponse := "error creating hotel"
+
+	a.NotNil(err)
+	a.Equal(expectedResponse, err.Error())
+
+}
+
+func TestInsertHotel(t *testing.T) {
+
+	a := assert.New(t)
+
+	hotelDto := dto.HotelDto{
+		Name:         "Hotel Test",
+		RoomAmount:   10,
+		Description:  "Hotel test description",
+		City:         "Test City",
+		StreetName:   "Test Street",
+		StreetNumber: 123,
+		Rate:         4.5,
+	}
+
+	hotelResponse, err := HotelService.InsertHotel(hotelDto)
+
+	hotelDto.Id = "654cf68d807298d99186019f"
+
+	a.Nil(err)
+	a.Equal(hotelDto, hotelResponse)
+
+}
+
+func TestGetHotelById_NotFound(t *testing.T) {
+
+	a := assert.New(t)
+
+	_, err := HotelService.GetHotelById("000000000000000000000000")
+
+	expectedResponse := "hotel not found"
+	a.NotNil(err)
+	a.Equal(expectedResponse, err.Error())
+}
+
+func TestGetHotelById_Found(t *testing.T) {
+
+	a := assert.New(t)
+
+	id := "654cf68d807298d99186019f"
+
+	hotelResponse, err := HotelService.GetHotelById(id)
+
+	a.Nil(err)
+	a.Equal(id, hotelResponse.Id)
+}
+
+func TestGetHotels(t *testing.T) {
+
+	a := assert.New(t)
+
+	hotelsResponse, err := HotelService.GetHotels()
+
+	a.Nil(err)
+
+	var emptyDto dto.HotelsDto
+	a.Equal(emptyDto, hotelsResponse)
+}
+
+func TestDeleteHotel_NotFound(t *testing.T) {
+
+	a := assert.New(t)
+
+	id := "000000000000000000000000"
+	err := HotelService.DeleteHotel(id)
+
+	a.NotNil(err)
+
+	expectedResponse := "hotel not found"
+	a.Equal(expectedResponse, err.Error())
+}
+
+func TestDeleteHotel_Found(t *testing.T) {
+
+	a := assert.New(t)
+
+	id := "654cf68d807298d99186019f"
+	err := HotelService.DeleteHotel(id)
+
+	a.Nil(err)
+}
+
+func TestUpdateHotel_NotFound(t *testing.T) {
+
+	a := assert.New(t)
+
+	id := "000000000000000000000000"
+
+	hotelDto := dto.HotelDto{Id: id}
+
+	_, err := HotelService.UpdateHotel(hotelDto)
+
+	a.NotNil(err)
+
+	expectedResponse := "hotel not found"
+	a.Equal(expectedResponse, err.Error())
+}
+
+func TestUpdateHotel_Found(t *testing.T) {
+
+	a := assert.New(t)
+
+	id := "654cf68d807298d99186019f"
+
+	hotelDto := dto.HotelDto{Id: id}
+
+	hotelResponse, err := HotelService.UpdateHotel(hotelDto)
+
+	a.Nil(err)
+	a.Equal(hotelDto, hotelResponse)
+}
