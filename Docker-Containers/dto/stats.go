81ab601d818578a583728a21526afe8775a8e550
MaximoChattas <maxichattas@gmail.com>
2023-11-25 01:11:02 -0300

    Container Stats dto created

---
+++ Docker-Containers/dto/stats.go
@@ -0,0 +1,11 @@
+package dto
+
+type ContainerStatsDto struct {
+	Id       string `json:"ID"`
+	Name     string `json:"Name"`
+	CPU      string `json:"CPUPerc"`
+	MemPerc  string `json:"MemPerc"`
+	MemUsage string `json:"MemUsage"`
+}
+
+type ContainersStatsDto []ContainerStatsDto
