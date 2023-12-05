fa91365cd0315198ee76b3c98898a346b765b175
MaximoChattas <maxichattas@gmail.com>
2023-12-05 14:44:24 -0300

    Auto Scaling function used for all services

--- Docker-Containers/main.go
+++ Docker-Containers/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"Docker-Containers/app"
+	"Docker-Containers/client"
 )
 
 func main() {
+
+	services := client.GetScalableServices()
+
+	for _, service := range services {
+
+		go client.AutoScale(service)
+
+	}
 
 	app.StartRoute()
 }
